a9e3945cc0252a5075815c4f3ea8d8208c710bca
EvgenijSeduh <128789239+EvgenijSeduh@users.noreply.github.com>
2023-11-27 18:17:01 +0300

    Add files via upload

--- finalshop/index.php
+++ finalshop/index.php
@@ -12,28 +12,36 @@ if ($_GET['user'] == 'autorization') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Главная</title>
     <link rel="stylesheet" href="styles.css">
+    <script src="slideshow.js"></script>
 </head>
 
 <body>
 
     <header>
-        <nav>
-            <ul>
-                <li><a href="index.php">Главная</a></li>
-                <li><a href="menu.php">Меню</a></li>
-                <li><a href="autorizate.php">Авторизация</a></li>
-                <?php
-                if ($flag == true) { ?>
-                    <li><a href="appeal.php">Обратная связь</a></li>
-                    <li><button id="basket">Корзина</button></li>
-                <?php } ?>
-            </ul>
-        </nav>
+        <?php
+        if ($flag == false) { ?>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Главная</a></li>
+                    <li><a href="menu.php">Меню</a></li>
+                    <li><a href="autorizate.php">Авторизация</a></li>
+                </ul>
+            </nav>
+        <?php } else {
+            include 'headerMenu.php';
+        }
+        ?>
     </header>
 
     <main>
         <h1>Добро пожаловать на Главную страницу</h1>
         <p>Приветствуем вас в нашем уютном ресторане, где каждый визит — это погружение в мир изысканной кухни и неповторимой атмосферы уюта. Наши шеф-повара с любовью готовят блюда из свежих ингредиентов, создавая настоящие кулинарные произведения искусства. В каждом бите наших блюд вы почувствуете тщательно подобранные вкусы, переносящие вас в путешествие по разнообразным кулинарным традициям. Приглашаем вас на уникальное гастрономическое приключение, где каждый момент становится особенным.</p>
+        <div id="slideshow">
+        <img class = "slide" src="img/slide1.jpg" alt="Slide 1">
+        <img class = "slide" src="img/slide2.jpg" alt="Slide 2">
+        <img class = "slide" src="img/slide3.jpg" alt="Slide 3">
+    </div>
+
     </main>
 
     <footer>
