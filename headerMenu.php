5a2eebda6c3e875c7d9b73907d132d926038f294
EvgenijSeduh <128789239+EvgenijSeduh@users.noreply.github.com>
2023-11-27 18:33:24 +0300

    Add files via upload

---
+++ headerMenu.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="menu.php">Меню</a></li>
+        <li><a href="appeal.php">Обратная связь</a></li>
+        <li><button id="basket">Корзина</button></li>
+    </ul>
+</nav>
---
+++ homeAutorizate.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        
+    <meta charset="UTF-8">
+        
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="../styles/styleHome.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Авторизация</title>
+</head>
+
+<body>
+    <header></header>
+    <main>
+        <div class="informWindow">
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $login = $_POST["login"];
+                $password = $_POST["password"];
+
+                $expectedLogin = "admin";
+                $expectedPassword = "admin";
+
+                if ($login === $expectedLogin && $password === $expectedPassword) {
+                    header("Location: index.php?user=autorization");
+                    header("Location: menu.php?user=autorization");
+                } else {
+                    echo "Неправильное имя пользователя или пароль.";
+                    echo '<p> </p><button onclick="redirectToOtherPage()">Вернуться назад</button>';
+                }
+            }
+            ?>
+        </div>
+    </main>
+    <footer></footer>
+    <script>
+        function redirectToOtherPage() {
+            window.location.href = "autorizate.php";
+        }
+    </script>
+
+</html>
